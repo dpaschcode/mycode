e846bbe07e91aa84847ea34b51e3492d3c18349a
DavidPasch <david.william.pasch@citi.com>
2022-11-07 19:41:02 +0000

    mix-list

---
+++ mix-list/mixlist02.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# display only the IP addresses to the screen.
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+# example 2 - use the comma separator
+print("IP addresses:\n", iplist[-2], "\n", iplist[-3])
