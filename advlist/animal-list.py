855a463be74077a45b9d47f145252fa510350667
DavidPasch <david.william.pasch@citi.com>
2022-11-07 21:41:41 +0000

    About Complext Lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def main():
+
+        # create a list of anmimals with 3 letter names
+        list1 = ["fox", "fly", "ant", "bee", "cod", "cat", "dog", "yak", "cow", "hen", "koi", "hog", "jay", "kit"]
+        print(list1)
+
+main()
