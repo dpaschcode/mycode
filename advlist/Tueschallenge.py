933edb2b989cf5c296645c3b383a586a89fd0ee9
DavidPasch <david.william.pasch@citi.com>
2022-11-08 14:45:57 +0000

    Update to Tuesday Chanllenge

--- advlist/Tueschallenge.py
+++ advlist/Tueschallenge.py
@@ -8,6 +8,14 @@ def main():
     user_firstname = input("first Name:")
     user_lastname = input("last name:")
     user_favnumber = input("favorite number:")
+    
+
+    # list1 = []      # "declaring"
+                      # list1 exsits and is the type list, but has a "zero value" (its empty)
+    # list1.append(x) # "initalization" - list1 has values the following "non-zero"
+    #
+    # 
+    # delcare & initalize at the same time
     list1 = [(user_firstname), (user_lastname), (user_favnumber)]
 
     # display info back to the user.
