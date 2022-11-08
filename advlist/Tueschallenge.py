de3161fadd051258f46f5e7a397189fcc07af7e4
DavidPasch <david.william.pasch@citi.com>
2022-11-08 14:22:24 +0000

    learning about input function

---
+++ advlist/Tueschallenge.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+""" script for Tuesday challenge"""
+
+def main():
+
+    # pause the program and wait for the user to provide input
+    user_firstname = input("first Name:")
+    user_lastname = input("last name:")
+    user_favnumber = input("favorite number:")
+    list1 = [(user_firstname), (user_lastname), (user_favnumber)]
+
+    # display info back to the user.
+    print(list1)
+
+# this calls main
+main()
