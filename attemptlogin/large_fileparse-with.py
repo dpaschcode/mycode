c1acc548ed808e4779b0ddb2f7631605952908aa
DavidPasch <david.william.pasch@citi.com>
2022-11-09 19:22:02 +0000

    new rc lab

--- attemptlogin/large_fileparse-with.py
+++ attemptlogin/large_fileparse-with.py
@@ -6,6 +6,8 @@
 loginfail = 0 # counter for fails
 successful = 0 # total times we see pattern, "-] Authorization failed"
 
+failedlogins = []
+
 # open the file for reading
 with open("/home/student/mycode/attemptlogin/keystone.common.wsgi") as kfile:
 
@@ -14,13 +16,15 @@ with open("/home/student/mycode/attemptlogin/keystone.common.wsgi") as kfile:
         # if this 'fail pattern' appears in the line...
         if "- - - - -] Authorization failed" in line:
             loginfail += 1 # this is the same as loginfail = loginfail + 1
+            failedlogins.append(line.split(" ")[-1].rstrip('\n'))
         # the following elif agumentation MUST be AFTER we check for a fail
         # this statement assumes the if statement above it tested false
         elif "-] Authorization failed" in line:  # can ONLY be true if the "if" was false!
             successful += 1 # this is the total number of times we see this pattern
 
 # display the number of failed log in attempts
-print("The number of failed login attempts is", loginfail, line.split(" ")[-1])
+print("The number of failed login attempts is", loginfail)
+print("List of IPs with failed logins:", failedlogins)
 
 # display the number of successful log in attempts
 print("The number of successful logins is", successful)
