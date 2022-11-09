b09ff6c41cd2813bde3e7dd62dc99c5cf9b60bb8
DavidPasch <david.william.pasch@citi.com>
2022-11-09 15:13:05 +0000

    Loop Labs

--- cust_if_script.py
+++ cust_if_script.py
@@ -4,21 +4,24 @@
 
 def main():
 
-    message1 = 'Hello!, Weilcome to the ice cream buffet!\n '
+    message1 = 'Hello!, Welcome to the ice cream buffet!\n '
     message2 = 'Call Weight Watchers!!! '
 
     # Gather user input and float to accept decimals as numbers
-    scoops = float(input("How many scoops do you want?"))
+    scoops = int(input("How many scoops do you want? "))
 
     # Analyze scoops for reply message
     if scoops == 1:
         message = message1 + 'Here is your ' + str(scoops) + ' scoop.'
     elif scoops == 2:
-        message = message1 + 'enjoy your ' + str(scoops) + '  scoops!.'
+        message = message1 + 'Enjoy your ' + str(scoops) + '  scoops!'
     elif scoops == 3:
         message = message1 + str(scoops) +'!!! Enough for a growing man!!!.'
     elif scoops > 3:
         message = message2
+    
+
+
     print(message)
 
 main()
