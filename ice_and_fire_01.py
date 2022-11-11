279c98d8d9aab630899755f998990ddd6df85838
DavidPasch <david.william.pasch@citi.com>
2022-11-11 17:08:52 +0000

    testing and pylint labs

--- ice_and_fire_01.py
+++ ice_and_fire_01.py
@@ -3,17 +3,17 @@
 # documentation for this API is at
 # https://anapioficeandfire.com/Documentation
 
-import requests
 import pprint
+import requests
 
 AOIF_CHAR = "https://www.anapioficeandfire.com/api/characters/"
 
 def main():
-    ## Ask user for input
-    got_charToLookup = input("Pick a number between 1 and 1000 to return info on a GoT character! ")
+    """Ask user for input"""
+    got_char_to_lookup = input("Pick a number between 1-1000 to return info on a GoT character! ")
 
     ## Send HTTPS GET to the API of ICE and Fire character resource
-    gotresp = requests.get(AOIF_CHAR + got_charToLookup)
+    gotresp = requests.get(AOIF_CHAR + got_char_to_lookup, timeout = 10)
 
     ## Decode the response
     got_dj = gotresp.json()
---
+++ smallscript.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+""" adding a doc string as a best practice"""
+
+MYNOTE = "print this string"
+print(MYNOTE)
